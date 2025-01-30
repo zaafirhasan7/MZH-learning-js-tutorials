d682a0a3919e21547aa71d3528f0e69fdb7e4b50
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-01-30 13:48:16 +0800

    slice without ending index

--- Jan 2025/29jan25.js
+++ Jan 2025/29jan25.js
@@ -1,3 +1,6 @@
 var longString = "My long string is long";
 var x = longString.slice(3,14); // not inclusive for ending index
-console.log (x);
+console.log (x);// long string
+
+var noEndPoint = longString.slice(3);
+console.log (noEndPoint);// long string is long
