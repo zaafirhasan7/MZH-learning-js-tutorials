2f05e2e1e0826cca061112f6b7f73c4d1ba7b631
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-01-30 15:00:14 +0800

    uppercase and lowercase

---
+++ Jan 2025/30jan25.js
@@ -0,0 +1,15 @@
+var lowerCase =  "hello there, i am zaafir";
+var upperCase = lowerCase.toUpperCase();
+// console.log (upperCase)
+
+var wibleWobble = "hELlo THERE, hOW ARE yOu doINg?"
+var lowerCase = wibleWobble.toLowerCase();
+// console.log (lowerCase);
+
+var question =  "hELlo THERE, hOW ARE yOu doINg?";
+var sliced_h = question.slice(0, 1);
+var upperCase_H = sliced_h.toUpperCase();
+var restOfText = question.slice(1);
+var lowerCaseRestOfText = restOfText.toLowerCase();
+var outputSentence = upperCase_H + lowerCaseRestOfText;
+console.log (outputSentence);
