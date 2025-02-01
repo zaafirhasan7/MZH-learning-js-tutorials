38aba807d0e5b89177b80dbb432980687e3840fb
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-02-01 21:28:17 +0800

    fixed output position(beside the console.log).

---
+++ feb 2025/1feb2025.js
@@ -0,0 +1,45 @@
+// greater than
+var height = 65;
+var heightRestriction = 60
+console.log (height > heightRestriction);// true
+
+var height = 60
+var heightRestriction = 60
+console.log (height > heightRestriction)// false
+
+// greater or equal to 
+var height = 60
+var heightRestriction = 60
+console.log (height >= heightRestriction)
+
+// less than
+var height = 60
+var heightRestriction = 48
+console.log(height < heightRestriction)
+
+// less or equal to
+var height = 48
+var heightRestriction = 48
+console.log (height <= heightRestriction);
+
+//triple equal to
+var mySecretNumber = 5;
+var TVmanGuess = 3;
+console.log (mySecretNumber === TVmanGuess); // false
+
+var cameramanGuess = 7;
+console.log (mySecretNumber === cameramanGuess); // false
+
+var speakermanGuess = 5;
+console.log (mySecretNumber === speakermanGuess) // true
+
+// double equals
+var stringNumber = "5";
+var actualNumber = 5;
+console.log (stringNumber === actualNumber);// false
+
+console.log (stringNumber == actualNumber);// true
+
+console.log (0 == false);// true
+
+console.log ("false" == true);// false 
