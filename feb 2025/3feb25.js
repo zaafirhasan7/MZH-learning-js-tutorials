cfb094691c52f0f4dfa90de86599e6ae6e4480b2
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-02-05 19:01:29 +0800

    starting arrays

---
+++ feb 2025/3feb25.js
@@ -0,0 +1,12 @@
+var myTopThreeCharacters = ["speakerman", "cameraman", "tvman"];
+console.log (myTopThreeCharacters)
+
+var dinosaur1 = "T-Rex";
+var dinosaur2 = "Velociraptor";
+var dinosaur3 = "Stegosaurus";
+var dinosaur4 = "Triceratops";
+var dinosaur5 = "Brachiosaurus";
+var dinosaur6 = "Pteranodon";
+var dinosaur7 = "Apatosaurus";
+var dinosaur8 = "Diplodocus";
+var dinosaur9 = "Compsognathus";
