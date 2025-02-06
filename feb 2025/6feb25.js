61f9301fcb48b3149df45f56c2ce35e13d6b278a
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-02-06 21:56:58 +0800

    adding elements with push, and unshift methods

---
+++ feb 2025/6feb25.js
@@ -0,0 +1,13 @@
+var animals = [];
+animals.push("cat"); // [0]
+animals[1]= "dog"; // [1]
+animals.push("llama"); // [2]
+animals.push("horse"); 
+console.log(animals);
+
+// unshift: adding an element at the first of the array
+ animals.unshift("monkey");
+ console.log(animals);
+ animals.unshift ("polar bear")
+ console.log(animals);
+
