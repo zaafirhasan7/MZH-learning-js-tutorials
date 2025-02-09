b9f5622808d0db7b2c5e0c92e6ca5bf487641726
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-02-09 22:06:08 +0800

    index of and join methods

---
+++ feb 2025/9feb25.js
@@ -0,0 +1,20 @@
+var colors = ["red", "dfdfd" ,"fdfdfd", "blue", "green", "blue"];
+
+var blueIdx = colors.indexOf("blue")
+console.log(blueIdx);
+
+var findingValueForBlue = colors[blueIdx];
+console.log(findingValueForBlue);
+
+
+// join method
+var separatedName = ["Mahedi", "Zaafir ♥"];
+var fullName = separatedName.join(" loves ");
+console.log(separatedName);
+console.log(fullName);
+
+var ages = [22, 11, 2013];
+var dob = ages.join("-"); 
+console.log(dob);
+
+
