ed37b465989c36c9dd6fb3ce5c7d42d5cc0d8ea8
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-02-10 22:13:15 +0800

    stack, math random, math floor methods

---
+++ feb 2025/10feb25.js
@@ -0,0 +1,33 @@
+var landmarks = [];
+landmarks.push("home")
+landmarks.push("masjid")
+landmarks.push("small restaurant")
+landmarks.push("madrassa")
+landmarks.push("al-faruque school")
+landmarks.push("bazaar tasty treat")
+landmarks.push("lions school")
+landmarks.push("cantt. public school")
+
+//heading back home
+// console.log(landmarks.pop()); 
+// console.log(landmarks.pop()); 
+// console.log(landmarks.pop()); 
+// console.log(landmarks.pop()); 
+// console.log(landmarks.pop()); 
+// console.log(landmarks.pop()); 
+// console.log(landmarks.pop());
+// console.log(landmarks.pop());
+
+// LIFO-> stack
+
+console.log(Math.random()*10);
+
+var random1 = Math.random()*10;
+var random2 = Math.random()*10;
+var random3 = Math.random()*10;
+console.log(random1,random1,random3);
+
+var random1_floor = Math.floor(random1);
+var random2_floor = Math.floor(random2);
+var random3_floor = Math.floor(random3);
+console.log(random1_floor,random2_floor,random3_floor);
