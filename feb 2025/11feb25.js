6fc187b16f00a81d1592187d5e5b1c955fde7ad6
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-02-13 22:27:03 +0800

    finding random index

---
+++ feb 2025/11feb25.js
@@ -0,0 +1,5 @@
+var randomWords = ["Explosion", "Cave", "Princess", "Pen"]
+var randomIndex = Math.floor(Math.random() * randomWords.length); // 0 , 1, 2 , 3
+console.log(randomWords[randomIndex]);
+
+ 
