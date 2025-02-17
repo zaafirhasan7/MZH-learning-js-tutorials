47708ac16cfc4b1db25b7823388a198536c9d40d
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-02-17 21:42:01 +0800

    accessing data from arrays and objects

---
+++ 17feb25.js
@@ -0,0 +1,8 @@
+var legoSets = [
+    {name:'Lego set 1', "total pcs":365, price:300},
+    {name:'Lego set 2', "total pcs":425, price:525},
+    {name:'Lego set 335', "total pcs":5545, price:1005}
+];
+console.log(legoSets[1].name, ',', legoSets[1]["total pcs"]); // Lego set 2 , 425
+console.log(legoSets[0].name, ',', legoSets[0]["total pcs"]);
+console.log(legoSets[2].name, ',', legoSets[2]["total pcs"]);
