e00bdcb7ab6bd73559061553c9b1a2418ab6c996
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-02-25 20:23:23 +0800

    if else if conditions

---
+++ feb 2025/25Feb25_if_else.js
@@ -0,0 +1,25 @@
+var lemonChicken = false;
+var beefWithBlackBean = false;
+var dimsum = false;
+
+var temp = "";
+if (lemonChicken) {
+    temp = "yay, i can have Lemon chicken!";
+} else if (beefWithBlackBean){
+    temp = "at least I can have beef";
+} else if (dimsum){
+    temp = "yay, my favorite";
+} else {
+    temp = "oh well, what could be wrong with fried rice?"
+}
+console.log(temp);
+
+
+
+// if (){
+
+// } else if (){
+
+// } else {
+
+// }
