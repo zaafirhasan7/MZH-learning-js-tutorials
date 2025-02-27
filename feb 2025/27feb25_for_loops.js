261955f25a52f92e9fca80f7d48a7678fa20d654
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-02-27 21:10:58 +0800

    basics of for loops

---
+++ feb 2025/27feb25_for_loops.js
@@ -0,0 +1,36 @@
+// Revising while loop and preventing infinite loops
+
+var x = 0; // int
+while (x < 10) {
+    // no need print always, instead the followings
+    // set of insturctions (code) 
+    // lines of code: 5  or 50 or .... any number
+    // console.log(x); // 0, ...., 9
+    x++; // must be int 
+}
+
+
+//! ########## FOR LOOPS ##########
+
+// for(setup variable; condition; increment){};
+for(var sheepICounted = 0; sheepICounted <= 3; sheepICounted++){
+//  console.log("I have counted " + sheepICounted +  " sheep!");
+};
+// console.log("Zzzzzzzzzz");
+
+// print 3 times "hello"
+for(var i = 0; i < 3; i++){
+    // console.log("hello");
+};
+
+// the food that you like most, print it 5 times,
+for(var i = 0; i < 5; i++){
+    console.log("shawarma");
+};
+
+// second most print it 3 times
+for(var i = 0; i < 3; i++) {
+    console.log("sushi");
+};
+
+
