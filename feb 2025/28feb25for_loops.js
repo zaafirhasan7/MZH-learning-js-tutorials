a26ee08e7c73ab2b1d13f2e9b66664862e0343f4
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-02-28 20:23:28 +0800

    some for loop challenges

---
+++ feb 2025/28feb25for_loops.js
@@ -0,0 +1,36 @@
+var animals = ["cat", "dog", "fish", "cow", "bird"]; // 5f
+
+for (var i = 0; i < animals.length; i++) {
+    // console.log("i have a " + animals[i] + " as a pet"); // 3
+}
+
+// console.log("i have a " + animals[0] + " as a pet"); // 3
+// console.log("i have a " + animals[1] + " as a pet"); // 3
+// console.log("i have a " + animals[2] + " as a pet"); // 3
+// console.log("i have a " + animals[3] + " as a pet"); // 3
+
+
+var name = 'zaafir';
+for (var i = 0; i < name.length; i++) {
+    // console.log(name[i]); 
+}
+
+
+for (var fun = 2; fun < 10000;fun = fun * 2) {
+    // console.log(fun);
+}
+
+var x = 3;
+while (x < 10000) {
+// console.log(x);
+x = x * 3;
+};
+
+var animals = ["Cat", "Fish", "Lemur", "Komodo Dragon"];
+for (var i = 0; i < animals.length; i++) {
+    animals[i] = 'Awesome ' + animals[i];
+}
+console.log(animals);
+
+
+
