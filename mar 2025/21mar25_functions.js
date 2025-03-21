aec07af60d6aed2f2f0f1500274e69e9117a5065
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-03-21 16:18:48 +0600

    addition and multiplication function

---
+++ mar 2025/21mar25_functions.js
@@ -0,0 +1,15 @@
+function add (num1,num2) {
+    return num1 + num2;
+};
+
+function multiply (num1,num2) {
+    return num1*num2;
+};
+
+var product = multiply(36325,9824);
+
+var result = add(product, 777);
+console.log(result);
+
+
+
