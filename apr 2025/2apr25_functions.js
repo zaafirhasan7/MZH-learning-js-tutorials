ca5887d3caf0cad643c5583879c3fb051806f95f
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-04-02 13:25:46 +0600

    learning inner and outer loops

---
+++ apr 2025/2apr25_functions.js
@@ -0,0 +1,12 @@
+var n = 3;
+for (var i = 0; i <= n; i++) {
+    console.log("i: huh, it's my turn! confirmed!" );
+    for (var j = 0; j <= n; j++) {
+       console.log("j: sending links, loading, confirmed!");
+       for (var k = 0; k <= n; k++) {
+           console.log("k: bzzzzz, working, downloading files, accessing, bzzz, done!");
+
+       }
+    }
+    
+}
