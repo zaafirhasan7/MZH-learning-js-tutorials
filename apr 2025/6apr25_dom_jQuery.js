aa6780028e13732e895f472ea4f7965618a13a11
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-04-06 11:36:37 +0600

    using jQuery in html

---
+++ apr 2025/6apr25_dom_jQuery.js
@@ -0,0 +1,13 @@
+var newMyMommaText = prompt("please give a momma heading:"); 
+$("#main-heading2").text(newMyMommaText);
+
+// add a paragraph inside the body
+$("body").append("<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem culpa veniam harum delectus, reprehenderit non blanditiis recusandae dolores facere, quos obcaecati beatae ratione fugiat accusamus ipsa architecto adipisci ut aperiam?</p>")
+$("body").append("<h2>end</h2>")
+
+// take fav hoby from 3 users and show them in website
+for (var i = 0; i < 3; i++) {
+    var favHobby = prompt ("enter your hobby!");
+
+}
+
