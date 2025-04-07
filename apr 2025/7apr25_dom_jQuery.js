761fe47a728eb035dc69959b60e78e920f6bbf95
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-04-07 14:34:52 +0600

    3 jQuery animation methods

---
+++ apr 2025/7apr25_dom_jQuery.js
@@ -0,0 +1,23 @@
+// var newMyMommaText = prompt("please give a momma heading:"); 
+// $("#main-heading2").text(newMyMommaText);
+
+
+// $("#main-heading").slideUp(1000).slideDown(1000);
+$("#main-heading2").hide().fadeIn(10000);
+// $("#main-heading2").text("My Mommma!").fadeOut(5000).fadeIn(2000);
+
+// for(var i = 1;i<=5;i++) {
+    // $("#main-heading2").hide(1000);
+    // $("#main-heading2").show(1000);
+// }
+// add a paragraph inside the body
+$("body").append("<p>this is a new paragraph</p>")
+$("p").hide(5000).show(5000);
+// $("body").append("<h2>end</h2>")
+
+// // take fav hoby from 3 users and show them in website
+// for (var i = 1; i <= 3; i++) {
+//     var favHobby = prompt ("enter your hobby!");
+//     $("body").append("<p>" + "hobby " + i + " " + favHobby + "</p>");        
+// }
+
