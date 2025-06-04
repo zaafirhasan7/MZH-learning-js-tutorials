97259ff98ae37356d0c34038c1853e517eca231a
Mohammed Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-06-04 13:45:16 +0800

    added new property speed in constructor and running draw method in constructor

--- June 2025/car_game.js
+++ June 2025/car_game.js
@@ -1,86 +1,91 @@
 class Car {
+  constructor(x, y, url, speed) {
+    this.x = x;
+    this.y = y;
+    this.url = url;
+    this.speed = speed;
 
-    constructor(x, y, url) {
-        this.x = x;
-        this.y = y;
-        this.url = url;
-    }
+    this.draw();
+  }
 
-    draw() {
-        var carHtml = `<img src = ${this.url}>`;
+  draw() {
+    var carHtml = `<img src = ${this.url}>`;
 
-        this.carElement = $(carHtml);
+    this.carElement = $(carHtml);
 
-        this.carElement.css({
-            position: "absolute",
-            left: this.x,
-            top: this.y,
-            // height: "250px",
-            // width: '450px',
-            // "object-fit": "cover"
-        });
+    this.carElement.css({
+      position: "absolute",
+      left: this.x,
+      top: this.y,
+      // height: "250px",
+      // width: '450px',
+      // "object-fit": "cover"
+    });
 
-        $("body").append(this.carElement);
-    };
+    $("body").append(this.carElement);
+  }
 
-    moveRight () {
-        this.x += 5;
+  moveRight() {
+    this.x += this.speed;
 
-        this.carElement.css({
-            left: this.x,
-            top:this.y
-        });
-    };
+    this.carElement.css({
+      left: this.x,
+      top: this.y,
+    });
+  }
 
-    moveLeft () {
-        this.x -= 5;
+  moveLeft() {
+    this.x -= this.speed;
 
-        this.carElement.css({
-            left: this.x,
-            top:this.y
-        });
-    };
+    this.carElement.css({
+      left: this.x,
+      top: this.y,
+    });
+  }
 
-};
+  moveUp() {
+    this.y -= this.speed;
 
+    this.carElement.css({
+      left: this.x,
+      top: this.y,
+    });
+  }
+}
 
-
-
-
-
-
-var acura = new Car (20, 20, "http://nostarch.com/images/car.png");
-var nissan = new Car(100, 200, "http://nostarch.com/images/car.png");
+var acura = new Car(20, 20, "http://nostarch.com/images/car.png", 15);
+var nissan = new Car(100, 200, "http://nostarch.com/images/car.png", 10);
 // var apollo = new Car(10, 350, "https://www.motorbiscuit.com/wp-content/uploads/2021/11/Apollo-EVO-Apollo-3-scaled.jpeg")
 
-acura.draw();
-nissan.draw();
 // apollo.draw();
-
 
 // 5 != 100
 // 95 != 100
 // 100 != 100 False
 // while(acura.x !== nissan.x){
-    // acura.moveRight();
+// acura.moveRight();
 // }
 
-setInterval(()=>{
-    nissan.moveRight();
-}, 45)
-
+setInterval(() => {
+  nissan.moveRight();
+}, 45);
 
 // right arrow key used to move right side
-$(document).keydown((event)=>{
-    if (event.keyCode == 39){
-        acura.moveRight();
-    }
-})
+$(document).keydown((event) => {
+  if (event.keyCode == 39) {
+    acura.moveRight();
+  }
+});
 
 // left arrow key used to move left side
-$(document).keydown((event)=>{
-    if (event.keyCode == 37){
-        acura.moveLeft();
-    }
-})
+$(document).keydown((event) => {
+  if (event.keyCode == 37) {
+    acura.moveLeft();
+  }
+});
 
+$(document).keydown((event) => {
+  if (event.keyCode == 38) {
+    acura.moveUp();
+  }
+});
