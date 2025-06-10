e67b9a7e3c0e5137eb13639a11d844c279213467
Mohammed Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-06-10 12:30:09 +0800

    making different paths using moveTo and lineTo

--- June 2025/canvas.js
+++ June 2025/canvas.js
@@ -33,18 +33,34 @@ for (let index = 0; index < 3; index++) {
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
   ctx.fillStyle = colors[index];
-  ctx.fillRect(x_pos, 10, 50, 100);
+  // ctx.fillRect(x_pos, 10, 50, 100);
   x_pos += 50;
 }
-
+// draw Bangladesh flag
 var canvas = document.getElementById("canvas");
 var ctx1 = canvas.getContext("2d");
 ctx1.fillStyle = "Green";
-ctx1.fillRect(170, 10, 150, 100);
+// ctx1.fillRect(170, 10, 150, 100);
 
 ctx1.fillStyle = "red";
 ctx1.beginPath();
 // Math.PI is π (about 3.14159), which is 180 degrees.
 // Math.PI * 2 is 360 degrees.
-ctx1.arc(235, 60, 36, 0, Math.PI * 2); // x, y, radius, startAngle, endAngle
-ctx1.fill();
+ctx1.arc(235, 60, 32, 0, Math.PI * 2); // x, y, radius, startAngle, endAngle
+// ctx1.fill();
+
+var strokeContext = canvas.getContext("2d");
+strokeContext.strokeStyle = "darkred";
+strokeContext.lineWidth = 10;
+// strokeContext.strokeRect(10,250,100,50);
+
+// drawing turquoise x
+var pathCtx = canvas.getContext("2d");
+pathCtx.strokeStyle = "Turquoise";
+pathCtx.lineWidth = 4;
+pathCtx.beginPath();
+pathCtx.moveTo(1264 / 2, 600);
+pathCtx.lineTo(1264 / 2, 0);
+pathCtx.moveTo(0, 600 / 2);
+pathCtx.lineTo(1264, 600 / 2);
+pathCtx.stroke();
