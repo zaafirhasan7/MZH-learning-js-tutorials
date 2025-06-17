3b1893b15a5aa7073c435113a98c81a15a903426
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-06-17 20:22:20 +0600

    making a stickman(complete)

--- June 2025/stickman.js
+++ June 2025/stickman.js
@@ -12,9 +12,14 @@ context.moveTo(200+(20/2), 10+20);
 context.lineTo(200+(20/2), (10+20)+45);
 
 // arms
-
+context.moveTo(210, 50);
+context.lineTo(192,38) // left arm
+context.moveTo(210, 50);
+context.lineTo((210-192)+210,38) // right arm
 
 // legs
-
-
+context.moveTo(210,75)
+context.lineTo(195, 105)// left leg
+context.moveTo(210,75)
+context.lineTo((210-195)+210, 105) // right leg
 context.stroke();
