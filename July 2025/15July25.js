cda148afb87c330c2295ab6103750c8aa24adf9f
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-07-15 14:34:00 +0600

    made a moutain view and a house in canvas using 2d context

---
+++ July 2025/15July25.js
@@ -0,0 +1,29 @@
+
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+// Making a mountain view art
+// ctx.fillStyle = "SkyBlue";
+// ctx.beginPath();
+// ctx.moveTo(100, 100);
+// ctx.lineTo(130,40);
+// ctx.lineTo(160,90);
+// ctx.lineTo(190,40);
+// ctx.lineTo(220,90);
+// ctx.lineTo(245,30);
+// ctx.lineTo(275,110)
+// ctx.lineTo(100,100);
+// ctx.fill();
+
+ctx.fillStyle = "SkyBlue";
+ctx.beginPath();
+ctx.moveTo(100,100);
+ctx.lineTo(100,40);
+ctx.lineTo(140,20);
+ctx.lineTo(180,40);
+ctx.lineTo(180,100);
+ctx.lineTo(100,100);
+ctx.fill();
+
+
