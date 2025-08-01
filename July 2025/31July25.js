ca33e094a8393d6bea803de20d18ec5358a99cd8
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-08-01 15:45:20 +0600

    modified hangman draw function with variable x and y coordinate value

--- July 2025/31July25.js
+++ July 2025/31July25.js
@@ -2,58 +2,44 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 function hangmanDrawer (mistakes) {
+    console.log("miastake coming from outside: " + mistakes);
+    var x = 200;
+    var y = 50;
     if (mistakes==1) {
         // draw head
         ctx.beginPath();
         ctx.lineWidth = 10;
-        ctx.strokeRect(250, 250, 50, 50);        
+        ctx.strokeRect(x, y, 50, 50);        
     } else if (mistakes==2) {
-        hangmanDrawer(1);
         // draw torso
         ctx.lineWidth = 10;
-        ctx.moveTo(250+50/2, 300);
-        ctx.lineTo(250+50/2, 450);
+        ctx.moveTo(x+50/2, y+50);
+        ctx.lineTo(x+50/2, y+150);
         ctx.stroke();
     } else if (mistakes==3) {
-        hangmanDrawer(1);
-        hangmanDrawer(2);
         // draw left hand
         ctx.lineWidth = 10;
-        ctx.moveTo(250+50/2,300+25);
-        ctx.lineTo((250+50/2)-75, (300+25)+65);
+        ctx.moveTo(x+50/2,y+75);
+        ctx.lineTo((x+50/2)-75, (y+75)+65);
         ctx.stroke();
     } else if (mistakes==4) {
-        hangmanDrawer(1);
-        hangmanDrawer(2);
-        hangmanDrawer(3);
         // draw right hand
         ctx.lineWidth = 10;
-        ctx.moveTo(250+50/2,300+25);
-        ctx.lineTo((250+50/2)+75, (300+25)+65);
+        ctx.moveTo(x+50/2,y+75);
+        ctx.lineTo((x+50/2)+75, (y+75)+65);
         ctx.stroke();
     } else if (mistakes==5) {
-        hangmanDrawer(1);
-        hangmanDrawer(2);
-        hangmanDrawer(3);
-        hangmanDrawer(4);
         // draw left leg
         ctx.lineWidth = 10;
-        ctx.moveTo(250+50/2,450);
-        ctx.lineTo((250+50/2)-70, (300+25)+200);
+        ctx.moveTo(x+50/2,y+150);
+        ctx.lineTo((x+50/2)-70, (y+25)+200);
         ctx.stroke();
     } else if (mistakes==6) {
-        hangmanDrawer(1);
-        hangmanDrawer(2);
-        hangmanDrawer(3);
-        hangmanDrawer(4);
-        hangmanDrawer(5);
         // draw right leg
         ctx.lineWidth = 10;
-        ctx.moveTo(250+50/2,450);
-        ctx.lineTo((250+50/2)+70, (300+25)+200);
+        ctx.moveTo(x+50/2,y+150);
+        ctx.lineTo((x+50/2)+70, (y+25)+200);
         ctx.stroke();
     }
 
 };
-
-hangmanDrawer(6);
