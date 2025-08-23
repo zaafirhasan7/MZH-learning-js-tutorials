6c6119d1437f8193ba3cef553a183ec347c349b1
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-08-23 12:18:25 +0600

    made bouncing ball with changing colours during collision

--- Aug 2025/21aug25_ball.js
+++ Aug 2025/21aug25_ball.js
@@ -7,7 +7,7 @@ class Ball {
     };
 
     draw (radius) {
-        circle(this.x, this.y, radius, "darkGreen", true);
+        circle(this.x, this.y, radius, true);
     };
 
     move () {
@@ -15,29 +15,52 @@ class Ball {
         this.y = this.y + this.ySpeed;
     };
 
+    checkCollision () {  
+
+        var randColor = generateRandomRGBColor();
+        console.log(randColor);
+        
+        if (this.x == 0 || this.x == 200) {
+            ctx.fillStyle = randColor;
+            this.xSpeed = -this.xSpeed;
+        };
+
+        if (this.y == 0 || this.y == 200) {
+            ctx.fillStyle = randColor;
+            this.ySpeed = -this.ySpeed;
+        };
+    };
+
 };
 
-function circle(x, y, radius, colour, isFilled) {
-    if (colour === undefined) {
-        colour = "black";
-    };
+function circle(x, y, radius, isFilled) {
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2*Math.PI, false);
     if(!isFilled){
-        ctx.strokeStyle = colour;
         ctx.stroke();
     } else {
-        ctx.fillStyle = colour;
         ctx.fill();
     };
 };
+
+function generateRandomRGBColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`; // rgb(118, 10, 118)
+}
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 ctx.strokeRect(0,0,200,200);
 
-var ball = new Ball(100, 100, -2, 3);
-ball.draw(3);
+var ball = new Ball(100, 100, -2,4);
 
  
-
+setInterval(()=> {
+    ctx.clearRect(0,0,200,200);
+    ctx.strokeRect(0,0,200,200);
+    ball.draw(6);
+    ball.move();
+    ball.checkCollision();
+}, 30)
