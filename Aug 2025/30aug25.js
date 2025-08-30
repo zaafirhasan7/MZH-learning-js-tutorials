86ef4b1fb3f454d1fc501e6ff66b090f238e37c2
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-08-30 11:20:40 +0600

    worked on event listener and event handler

---
+++ Aug 2025/30aug25.js
@@ -0,0 +1,9 @@
+function keyDownHandler (event) {
+    console.log(event.key);
+};
+
+document.addEventListener("keydown", keyDownHandler);
+
+// adding an event listener
+// identifying event name
+// adding the event handler function
