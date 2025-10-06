3c499c043c290df6edbea191aa678463d516f5ec
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-10-06 11:08:04 +0600

    made move method for the snake (in progress)

--- Sep 2025/Snake.js
+++ Sep 2025/Snake.js
@@ -16,4 +16,19 @@ class Snake {
         };
     };
 
+    move () {
+        var head = this.segments[0];
+        var newHead;
+        this.direction = this.nextDirection;
+        if (this.direction === "right") {
+            newHead = new Block (head.col+1, head.row);
+        } else if (this.direction === "left") {
+            newHead = new Block (head.col - 1, head.row);
+        } else if (this.direction === "up") {
+            newHead = new Block (head.col, head.row-1);
+        } else if (this.direction === "down") {
+            newHead = new Block (head.col, head.row+1);
+        }
+    };
+
 };
