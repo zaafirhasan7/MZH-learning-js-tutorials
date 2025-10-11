af204b9108e2784cf465f9e98c972afe6208c9b6
Zaafir Hasan <mohammedzaafirhasan@outlook.com>
2025-10-11 10:34:38 +0600

    move method logic

--- Sep 2025/Apple.js
+++ Sep 2025/Apple.js
@@ -6,4 +6,23 @@ class Apple{
     draw (ctx, blockSize) {
         this.position.drawCircle(ctx, blockSize, "limeGreen")
     }
+
+    move () {
+        // setting a random column and row position for the apple when it is eaten by the snake
+
+        // find a random col
+        // find a random row
+        // use the random col and row to make a new block instance and 
+        // use that instance to set the apple's position (correct, CLEAR)
+        
+        // lowest case  = 0
+        // highest case = 0.99
+
+
+
+
+
+
+
+    }
 }
